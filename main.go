dd66f66c01a15935b25cfe9f24105a98d1b5aa5a
Axel Bock <ab@a3b3.de>
2024-04-28 11:02:54 +0200

    feat(redirecting): working implementation

--- main.go
+++ main.go
@@ -4,34 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 // cannot use ":=" here, see https://stackoverflow.com/a/50875851/902327
 var listen_port = 8081
 
-func redirect_path(w http.ResponseWriter, r *http.Request) {
-	info := fmt.Sprintf("REDIRECT 'PATH': %s\n", r.URL.Path)
-	fmt.Printf(info)
-	fmt.Fprintf(w, info)
+func redirect_xxx(w http.ResponseWriter, r *http.Request, redirect_method int) {
+	u, _ := url.Parse(mux.Vars(r)["url"])
+	u.RawQuery = r.URL.RawQuery
+	u.RawFragment = r.URL.RawFragment
+	redirect_target := fmt.Sprintf("//%s", u.String())
+
+	fmt.Printf("[DEBUG] redirect_target=%s\n", redirect_target)
+	http.Redirect(w, r, redirect_target, redirect_method)
 }
 
-func redirect_rest(w http.ResponseWriter, r *http.Request) {
-	rest := mux.Vars(r)["rest"]
-	info := fmt.Sprintf("REDIRECT 'REST': %s\n", rest)
-	fmt.Printf(info)
-	fmt.Fprintf(w, info)
+func redirect_301(w http.ResponseWriter, r *http.Request) {
+	redirect_xxx(w, r, http.StatusMovedPermanently)
 }
 
 func index(w http.ResponseWriter, _ *http.Request) {
 	fmt.Printf("Handler for / called\n")
 	fmt.Fprintf(w, "Information forthcoming")
-}
-
-func handle_test(w http.ResponseWriter, _ *http.Request) {
-	fmt.Printf("Handler for /test called\n")
-	fmt.Fprintf(w, "Test handler\n")
 }
 
 func main() {
@@ -41,23 +39,11 @@ func main() {
 	// note we're using the gorilla.mux router
 	r := mux.NewRouter()
 
-	// in "basic" examples, we use "http.HandleFunc()", but not here.
-	// this is so good.
-	// HandlerFunc is a _type_ !!  (see https://www.willem.dev/articles/http-handler-func/)
-	// what we do here is a type conversion, and the resulting type ("HandlerFunc")
-	// has a member function "ServeHTTP(w, r)", which simply calls "redirect(w, r)"
-	// so, basically, a function with a member function calling the actual function.
-	// (this here works very much like a python decorator)
-	r.PathPrefix("/-").
-		Handler(
-			http.StripPrefix(
-				"/-/",
-				http.HandlerFunc(redirect_path)))
+	// https://stackoverflow.com/a/49312740/902327
+	r.Use(handlers.ProxyHeaders)
 
-	// this works too, but we want to strip the prefix before that.
-	//r.PathPrefix("/-").HandlerFunc(redirect)
-
-	r.HandleFunc("/r/{rest:.+}", redirect_rest)
+	// that's all we need?
+	r.HandleFunc("/{marker:[^/]+}/{url:.+}", redirect_301)
 
 	// catch-all
 	r.HandleFunc("/", index)
